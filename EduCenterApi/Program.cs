58fd04db3c88916d6c4fd45c82fbc926376f0933
ruslanmr25 <dilshodahmedovvv@gmail.com>
2025-06-17 19:00:53 +0500

    Main

--- EduCenterApi/Program.cs
+++ EduCenterApi/Program.cs
@@ -8,11 +8,22 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowBlazorClient", policy =>
+    {
+        policy.WithOrigins("http://localhost:5228") // bu sizning Blazor frontend manzilingiz
+              .AllowAnyHeader()
+              .AllowAnyMethod();
+    });
+});
+
 builder.Services.AddSwaggerGen();
 
 builder.Services.RegisterApplicationServices();
 builder.Services.RegisterInfrastructureServces(
-    connectionString: builder.Configuration.GetConnectionString("DefaultConnectionString")
+    connectionString: builder.Configuration.GetConnectionString("DefaultConnectionString") ?? throw new NullReferenceException("Connection string not founf")
     );
 
 
@@ -25,7 +36,11 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+
+
 app.UseHttpsRedirection();
+
+app.UseCors("AllowBlazorClient");
 
 app.UseAuthorization();
 
